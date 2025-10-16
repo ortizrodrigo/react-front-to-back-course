b320bbdd524ad41f5f5f2e0a27759ffdcdb4ee7d
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2025-10-15 17:58:49 -0700

    Feedback App: Create Header component and add main styles

--- feedback-app/src/App.jsx
+++ feedback-app/src/App.jsx
@@ -1,27 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  const title = "Blog Post";
-  const body = "This is my Blog Post";
-  const comments = [
-    { id: 1, text: "Comment one" },
-    { id: 2, text: "Comment two" },
-    { id: 3, text: "Comment three" },
-  ];
-
   return (
-    <div>
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-      <div className="comments">
-        <h3>Comments ({comments.length})</h3>
-        <ul>
-          {comments.map((comment, index) => (
-            <li key={index}>{comment.text}</li>
-          ))}
-        </ul>
+    <>
+      <Header />
+      <div className="container">
+        <h1>My App</h1>
       </div>
-    </div>
+    </>
   );
 }
 
